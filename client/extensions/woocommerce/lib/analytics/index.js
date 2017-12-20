d7f14fe3bd53f9805918b6625230a08ba5234033
Justin Shreve <justin.shreve@gmail.com>
2017-12-20 11:14:45 -0800

    Attempt at discarding track events for certain flagged sites

--- client/extensions/woocommerce/lib/analytics/index.js
+++ client/extensions/woocommerce/lib/analytics/index.js
@@ -10,7 +10,19 @@ import debugFactory from 'debug';
  */
 import analytics from 'lib/analytics';
 import * as tracksUtils from './tracks-utils';
-
+import { getBlogStickers } from 'state/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
 const debug = debugFactory( 'woocommerce:analytics' );
 
-export const recordTrack = tracksUtils.recordTrack( analytics.tracks, debug );
+export const tracksStore = {
+	getBlogStickers: function() {
+		const state = tracksStore.reduxStore.getState();
+		const siteId = getSelectedSiteId( state );
+		return getBlogStickers( state, siteId );
+	},
+	setReduxStore( reduxStore ) {
+		this.reduxStore = reduxStore;
+	},
+};
+
+export const recordTrack = tracksUtils.recordTrack( analytics.tracks, debug, tracksStore );
--- client/extensions/woocommerce/lib/analytics/tracks-utils.js
+++ client/extensions/woocommerce/lib/analytics/tracks-utils.js
@@ -3,15 +3,21 @@
 /**
  * External dependencies
  */
-import { startsWith } from 'lodash';
+import { startsWith, includes } from 'lodash';
 
-export const recordTrack = ( tracks, debug ) => ( eventName, eventProperties ) => {
+export const recordTrack = ( tracks, debug, tracksStore ) => ( eventName, eventProperties ) => {
 	if ( ! startsWith( eventName, 'calypso_woocommerce_' ) ) {
 		debug( `invalid store track name: '${ eventName }', must start with 'calypso_woocommerce_'` );
 		return;
 	}
 
 	debug( `track '${ eventName }': `, eventProperties );
+
+	const blogStickers = tracksStore.getBlogStickers();
+	if ( includes( blogStickers, 'dotcom-store-test-site' ) ) {
+		debug( 'track request discarded. this site is flagged with `dotcom-store-test-site`' );
+		return;
+	}
 
 	tracks.recordEvent( eventName, eventProperties );
 };
