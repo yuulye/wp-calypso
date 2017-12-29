1e64148723d804f3d1aa2148b6fbbf2190a71018
Copons <copons@users.noreply.github.com>
2017-12-29 12:41:35 +0000

    Add the order to the selector

--- client/state/selectors/get-comments-page.js
+++ client/state/selectors/get-comments-page.js
@@ -3,7 +3,7 @@
  * External dependencies
  */
 
-import { get } from 'lodash';
+import { get, includes } from 'lodash';
 
 /**
  * Returns a list of comment IDs for the requested page and filters.
@@ -11,15 +11,22 @@ import { get } from 'lodash';
  * @param {Object} state Redux state.
  * @param {Number} siteId Site identifier.
  * @param {Object} query Filter parameters.
+ * @param {String} [query.order] Query order ('ASC' or 'DESC').
  * @param {Number} [query.page] Requested page.
  * @param {Number} [query.postId] Post identifier.
  * @param {String} [query.search] Search query.
  * @param {String} [query.status] Comments status.
  * @returns {Array} List of comment IDs for the requested page and filters.
  */
-export const getCommentsPage = ( state, siteId, { page = 1, postId, search, status = 'all' } ) => {
+export const getCommentsPage = (
+	state,
+	siteId,
+	{ order, page = 1, postId, search, status = 'all' }
+) => {
 	const parent = postId || 'site';
-	const filter = !! search ? `${ status }?s=${ search }` : status;
+	const orderFilter = includes( [ 'ASC', 'DESC' ], order ) ? order : 'DESC';
+	const searchFilter = !! search ? `&s=${ search }` : '';
+	const filter = `${ status }?order=${ orderFilter }${ searchFilter }`;
 	return get( state, [ 'ui', 'comments', 'queries', siteId, parent, filter, page ], [] );
 };
 
