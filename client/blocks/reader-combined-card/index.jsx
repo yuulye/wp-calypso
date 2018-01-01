6674fbd2a0667f469bc7da01dc6a47d0c754a44a
yuulye <yuulye@gmail.com>
2018-01-02 00:08:02 +0700

    update

--- client/blocks/reader-combined-card/index.jsx
+++ client/blocks/reader-combined-card/index.jsx
@@ -86,55 +86,55 @@ class ReaderCombinedCard extends React.Component {
 		const mediaCount = filter( posts, post => ! isEmpty( post.canonical_media ) ).length;
 
 		var white = [
-			74221510,
-			69435401,
-			68672991,
-			40806253,
-			36495360,
-			73453524,
-			63006096,
-			73868163,
-			65985495,
-			24986611,
-			71666708,
-			67681209,
-			72190224,
-			382486,
-			33336689,
-			76555404,
-			61094332,
-			76004000,
-			75634817,
-			71725908,
-			59180997,
-			76045835,
-			72732533,
-			73929580,
-			69922209,
-			74672960,
-			68464086,
-			76141811,
-			52949380,
-			59459306,
-			46544872,
-			68804394,
-			69419855,
-			64756695,
-			59654271,
-			68821911,
-			75424596,
-			70129411,
-			66756476,
-			35170000,
-			46338578,
-			74005977,
-			76665336,
-			76122503,
-			76221427,
-			69840324,
-			66565216,
-			68666696,
-			75255412,
+			69435401, // ~ cogitoesoterica - smalltimejetsetter - cogitoesoterica - https://smalltimejetsetter.wordpress.com
+			40806253, // ~ patrickstories247 - PATRICK REAL STORIES - PATRICK STORIES - https://patrickrealstories.wordpress.com
+			73453524, // ~ naveed7493 - Women Diaries07 - Mariam Naveed - https://womendiaries07.wordpress.com
+			63006096, // ~ zovision782 - Zovision - Zovi - https://zovisionsite.wordpress.com
+			64756695, // ~ agp98 - A SPARK - agp98 - https://agp98.wordpress.com
+			74221510, // ~ theunstormingmind - The Storming Mind - thestormingmind - https://thestormingmind.wordpress.com
+			59459306, // ~ doubledacres -                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            doubledacres - doubledacres - http://ramblingsofafarrier.com
+			71666708, // ~ angelamcclintock - Water for Camels - angelamcclintock - http://waterforcamels.com
+			73868163, // ~ iscriblr - iScriblr - iScriblr - http://iscriblr.com
+			// 382486, // ~ bizmarc - Die Erste Eslarner Zeitung - Aus und über Eslarn, sowie die bayerisch-tschechische Region! - OIKOS™-Redaktion - https://bizmarc.wordpress.com
+			75424596, // ~ jondwx - jondwx - jondwx - https://jondwx.wordpress.com
+			24986611, // ~ dee02kay - The Immortal Arts by Dee Kay - Dee Kay - https://imdeekay.wordpress.com
+			36495360, // ~ harithsoman - harithsnair - Harith S Nair - https://harithsnair.wordpress.com
+			75255412, // ~ kishwarayesha - The Write Direction - Ayesha☆Kishwar - https://blog26989.wordpress.com
+			66756476, // ~ thekintsukuroilife - The Kintsukuroi Life - thekintsukuroilife - https://thekintsukuroilife.wordpress.com
+			67681209, // ~ ivestrendytopic - Ive's Trendy Topic - IvesTrendyTopic - https://ivestrendytopic.wordpress.com
+			74672960, // ~ objectcodercom - Sanjib.org - Sanjib Ahmad - http://sanjib.org
+			65985495, // ~ truthiskef - truthiskef - Rohini - https://truthiskef.wordpress.com
+			69419855, // ~ szshaikh - Islamic Gems - SZ - https://szshaikh.wordpress.com
+			73929580, // ~ gunjaninks - GunjanInks - gunjaninks - https://gunjaninks.wordpress.com
+			70129411, // ~ simofutet - Foolproof Musings Of A Weirdie - Sim - https://simofutet.wordpress.com
+			75634817, // ~ bebloggerofficial - Be Blogger (Official) - BeBloggerofficial - http://bebloggerofficial.com
+			68672991, // ~ selfmotivationisinspiration - Feelings Do Motivate Too - Ritika Nijhawan - https://selfmotivationisinspiration.wordpress.com
+			61094332, // ~ nildamacedopaulino - My Life - nildamacedopaulino - https://nildamacedopaulino.wordpress.com
+			76004000, // ~ viviensvoice - Viviensvoice - Vivien Ayinotu - http://viviensvoice.com
+			72190224, // ~ pennylanethoughts - Penny Wilson Writes - Penny Wilson Writes - http://pennywilsonwrites.com
+			68804394, // ~ wanderingwaffles - Wandering Waffles - Wandering Waffles - http://wanderingwaffles.com
+			59654271, // ~ dystopiancitznblog - DystopianCitzn Blog - Ayah - https://dystopiancitznblog.wordpress.com
+			69922209, // ~ booknerdtravels - A Booknerd Travels - Annie - https://booknerdtravels.wordpress.com
+			46544872, // ~ fictiontoactual - Actualize Dreams - David Web Designer - https://fictiontoactual.wordpress.com
+			76555404, // ~ appletomypiee - appletomypiee - appletomypiee - https://appletomypiee.wordpress.com
+			59180997, // ~ daisymae2017 - WELCOME TO CRYSTAL'S SITE(ORIGINALLY COUNTRY LIVING) - Crystal Stewart - https://daisymae2017.wordpress.com
+			76045835, // ~ lisayoungphotographyportfolio - Bella Capture - L Young - http://bellacapture.net
+			76665336, // ~ ourbeautifullies - Our Beautiful Lies - ourbeautifullies - http://ourbeautifullies.com
+			// 66565216, // ~ kimberlyfren - This Girl's Got Curves - kimberlyf - https://thisgirlsgotcurves.wordpress.com
+			76141811, // ~ crawlingtocoding - Crawling to Coding - crawlingtocoding - https://crawlingtocoding.wordpress.com
+			52949380, // ~ nsmtechnicals - Nsm.Technicals - Suraj singh - https://nsmtechnicals.wordpress.com
+			46338578, // ~ yaskhan66 - yaskhan - yassy - https://yassy66.wordpress.com
+			68821911, // ~ dhilshaa -  "The Interminable Thoughts✍️" - Dhilshaa❤ - https://dhilshaa.wordpress.com
+			76221427, // ~ nvinside - CK’s Technology News - CHEF-KOCH - https://chefkochblog.wordpress.com
+			68464086, // ~ egates49yahoocom - Eddie Hotel - Ed "Hotel" Gates - https://eddiehotel27.wordpress.com
+			33336689, // ~ omabdalrahmaan - Try to get it! - Sohair - https://omabdalrahmaan.wordpress.com
+			72732533, // ~ webworksdesigners - ProgWiz - WebWorks - https://webworksdesigners.wordpress.com
+			68666696, // ~ mysticwriter2002 - poetic essence - mysticwriter2002 - https://poetricessence.wordpress.com
+			76122503, // ~ yuulye - A Qoder Blog ✍️ - yuulye - https://yuulye.wordpress.com
+			74005977, // ~ sachinsnagaraj - SN Designs - sachinsnagaraj - https://sachinnagaraja.wordpress.com
+			// 35170000, // ~ tonyburgess1969 - The Tony Burgess Blog - Tony Burgess - http://tonyburgess1969.net
+			69840324, // ~ touchmyspinebookreviews - Touch My Spine Book Reviews  - Dani☆Touch My Spine Book Reviews☆ - http://touchmyspinebookreviews.com
+			71725908, // ~ innalearnsjs - Inna Learns JS - innalearnsjs - https://innalearnsjs.wordpress.com
 		];
 
 		var found = false;
